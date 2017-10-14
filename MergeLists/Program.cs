64cfb8239cd6202b040e30a2d206fff6d7369f49
shreeharshas <s.shreeharsha@gmail.com>
2017-10-14 16:48:40 -0500

    TODO: need to assign ans to a' head before returning it

--- MergeLists/Program.cs
+++ MergeLists/Program.cs
@@ -9,9 +9,9 @@ namespace MergeLists
   //Definition for singly-linked list.    
   public class ListNode
     {
-      public int val;
-      public ListNode next;
-      public ListNode(int x) { val = x; }
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
         public override string ToString()
         {
             ListNode t = this;
@@ -19,9 +19,11 @@ namespace MergeLists
             while (t!=null)
             {
                 sb.Append(t.val);
+                sb.Append("->");
                 t = t.next;
             }
-            return sb.ToString();
+            
+            return sb.ToString().TrimEnd(new char[] { '-','>'});
         }
     }
 
@@ -39,60 +41,75 @@ namespace MergeLists
             ListNode l22 = new ListNode(4);
             l21.next = l22;
             ListNode l23 = new ListNode(6);
-            l23.next = l23;
-
-            Console.WriteLine(MergeTwoLists(l11,l21).ToString());
+            l22.next = l23;
+            Console.WriteLine("Input List 1:" + l11.ToString());
+            Console.WriteLine("Input List 2:" + l21.ToString());
+            ListNode res = MergeTwoLists(l11, l21);
+            Console.WriteLine("Merged List:" + (res == null ? string.Empty : res.ToString())+"\n--END--");
             Console.ReadKey();
         }
-        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2, bool isUniqueConstraint = false)
         {
-            ListNode a = new ListNode(Math.Min(l1.val, l2.val));
-
-            ListNode p1 = l1.next;
-            ListNode p2 = l2.next;
-            ListNode l = a;
-
-            bool isl1over, isl2over;
-
-            while (p1 != null || p2 != null)
+            ListNode a = null, p1 = l1, p2 = l2, ans = a;
+            while (true)
             {
-                isl1over = (p1 == null);
-                isl2over = (p2 == null);
+                if (p1 == null && p2 == null)
+                    return ans;
 
-                if (!isl1over && !isl2over)
+                else if (p1 != null && p2 == null)
+                {
+                    a = new ListNode(p1.val)
+                    {
+                        next = null
+                    };
+                    p1 = p1.next;
+                    a = a.next;
+                }
+
+                else if (p1 == null && p2 != null)
+                {
+                    a = new ListNode(p2.val)
+                    {
+                        next = null
+                    };
+                    p2 = p2.next;
+                    a = a.next;
+                }
+
+                else
                 {
                     if (p1.val < p2.val)
                     {
-                        a.next = new ListNode(p1.val);
+                        a = new ListNode(p1.val)
+                        {
+                            next = null
+                        };
                         p1 = p1.next;
+                        a = a.next;
                     }
                     else if (p1.val > p2.val)
                     {
-                        a.next = new ListNode(p2.val);
+                        a = new ListNode(p2.val)
+                        {
+                            next = null
+                        };
                         p2 = p2.next;
+                        a = a.next;
                     }
+
                     else
                     {
-                        a.next = new ListNode(p2.val);
+                        a = new ListNode(p1.val)
+                        {
+                            next = new ListNode(p2.val)
+                        };
                         p1 = p1.next;
                         p2 = p2.next;
+                        a.next.next = null;
+                        a = a.next.next;
                     }
                 }
-
-                if (isl1over && !isl2over)
-                {
-                    l = new ListNode(p2.val);
-                    p2 = p2.next;
-                }
-
-                if (isl2over && !isl1over)
-                {
-                    l = new ListNode(p1.val);
-                    p1 = p1.next;
-                }
-                //l = l.next;
             }
-            return a;
         }
     }
 }
